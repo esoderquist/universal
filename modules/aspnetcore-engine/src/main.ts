d2cac3bcc932b3e1d165c11260562588703d0de7
Eric Soderquist <esoderq@gmail.com>
2018-01-06 15:40:37 -0900

    HACK: based on https://github.com/angular/universal/issues/835#issuecomment-355391223

--- modules/aspnetcore-engine/src/main.ts
+++ modules/aspnetcore-engine/src/main.ts
@@ -1,68 +1,144 @@
-import { Type, NgModuleFactory, CompilerFactory, Compiler } from '@angular/core';
-import { platformDynamicServer, BEFORE_APP_SERIALIZED, renderModuleFactory } from '@angular/platform-server';
+import { Type, NgModuleFactory, CompilerFactory, Compiler, ApplicationRef, StaticProvider } from '@angular/core';
+import { platformDynamicServer, PlatformState, INITIAL_CONFIG, platformServer, BEFORE_APP_SERIALIZED } from '@angular/platform-server';
 import { ResourceLoader } from '@angular/compiler';
 
 import { REQUEST, ORIGIN_URL } from './tokens';
 import { FileLoader } from './file-loader';
 
 import { IEngineOptions } from './interfaces/engine-options';
-import { DOCUMENT } from '@angular/platform-browser';
+
+import { filter } from 'rxjs/operator/filter';
+import { first } from 'rxjs/operator/first';
+import { toPromise } from 'rxjs/operator/toPromise';
 
 /* @internal */
 export class UniversalData {
-  public static appNode = '';
-  public static title = '';
-  public static scripts = '';
-  public static styles = '';
-  public static meta = '';
-  public static links = '';
+  public appNode = '';
+  public title = '';
+  public scripts = '';
+  public styles = '';
+  public meta = '';
+  public links = '';
 }
 
 /* @internal */
 let appSelector = 'app-root'; // default
 
 /* @internal */
-function beforeAppSerialized(
+export function _getUniversalData(
   doc: any /* TODO: type definition for Domino - DomAPI Spec (similar to "Document") */
-) {
+): UniversalData {
 
-  return () => {
-    const STYLES = [];
-    const SCRIPTS = [];
-    const META = [];
-    const LINKS = [];
+  const STYLES = [];
+  const SCRIPTS = [];
+  const META = [];
+  const LINKS = [];
 
-    for (let i = 0; i < doc.head.children.length; i++) {
-      const element = doc.head.children[i];
-      const tagName = element.tagName.toUpperCase();
+  for (let i = 0; i < doc.head.children.length; i++) {
+    const element = doc.head.children[i];
+    const tagName = element.tagName.toUpperCase();
 
-      switch (tagName) {
-        case 'SCRIPT':
-          SCRIPTS.push(element.outerHTML);
-          break;
-        case 'STYLE':
-          STYLES.push(element.outerHTML);
-          break;
-        case 'LINK':
-          LINKS.push(element.outerHTML);
-          break;
-        case 'META':
-          META.push(element.outerHTML);
-          break;
-        default:
-          break;
-      }
+    switch (tagName) {
+      case 'SCRIPT':
+        SCRIPTS.push(element.outerHTML);
+        break;
+      case 'STYLE':
+        STYLES.push(element.outerHTML);
+        break;
+      case 'LINK':
+        LINKS.push(element.outerHTML);
+        break;
+      case 'META':
+        META.push(element.outerHTML);
+        break;
+      default:
+        break;
     }
+  }
 
-    UniversalData.title = doc.title;
-    UniversalData.appNode = doc.querySelector(appSelector).outerHTML;
-    UniversalData.scripts = SCRIPTS.join(' ');
-    UniversalData.styles = STYLES.join(' ');
-    UniversalData.meta = META.join(' ');
-    UniversalData.links = LINKS.join(' ');
+  // ServerTransferStateModule.serializeTransferStateFactory() appends to body
+  for (let i = 0; i < doc.body.children.length; i++) {
+    const element: Element = doc.body.children[i];
+    const tagName = element.tagName.toUpperCase();
+
+    switch (tagName) {
+      case 'SCRIPT':
+        SCRIPTS.push(element.outerHTML);
+        break;
+      case 'STYLE':
+        STYLES.push(element.outerHTML);
+        break;
+      case 'LINK':
+        LINKS.push(element.outerHTML);
+        break;
+      case 'META':
+        META.push(element.outerHTML);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return {
+    title: doc.title,
+    appNode: doc.querySelector(appSelector).outerHTML,
+    scripts: SCRIPTS.join('\n'),
+    styles: STYLES.join('\n'),
+    meta: META.join('\n'),
+    links: LINKS.join('\n')
   };
+};
+
+/* @internal */
+export interface PlatformOptions {
+  document?: string;
+  url?: string;
+  extraProviders?: StaticProvider[];
 }
 
+/**
+ * renderModuleFactory2 returns a Domino document
+ * @param moduleFactory
+ * @param options
+ * @returns Domino type document TODO: type definition for Domino - DomAPI Spec (similar to "Document")
+ */
+export function renderModuleFactory2<T>(
+  moduleFactory: NgModuleFactory<T>, options: PlatformOptions): Promise<any> {
+
+  const extraProviders = options.extraProviders ? options.extraProviders : [];
+
+  const platform = platformServer([
+    { provide: INITIAL_CONFIG, useValue: { document: options.document, url: options.url } },
+    extraProviders
+  ]);
+
+  return platform.bootstrapModuleFactory(moduleFactory).then((moduleRef) => {
+
+    const applicationRef: ApplicationRef = moduleRef.injector.get(ApplicationRef);
+
+    return toPromise
+      .call(first.call(filter.call(applicationRef.isStable, (isStable: boolean) => isStable)))
+      .then(() => {
+
+        // Run any BEFORE_APP_SERIALIZED callbacks just before rendering to string.
+        const callbacks = moduleRef.injector.get(BEFORE_APP_SERIALIZED, null);
+        if (callbacks) {
+          for (const callback of callbacks) {
+            try {
+              callback();
+            } catch (e) {
+              // Ignore exceptions.
+              console.warn('Ignoring BEFORE_APP_SERIALIZED Exception: ', e);
+            }
+          }
+        }
+        const state: PlatformState = platform.injector.get(PlatformState);
+        const output = state.getDocument();
+        platform.destroy();
+        return output;
+      });
+  });
+}
 
 export function ngAspnetCoreEngine(
   options: IEngineOptions
@@ -94,39 +170,38 @@ export function ngAspnetCoreEngine(
 
       options.providers = options.providers || [];
 
-      const extraProviders = options.providers.concat(
+      const extraProviders: StaticProvider[] = options.providers.concat(
         ...options.providers,
-          [{
-            provide: ORIGIN_URL,
-            useValue: options.request.origin
-          }, {
-            provide: REQUEST,
-            useValue: options.request.data.request
-          }, {
-            provide: BEFORE_APP_SERIALIZED,
-            useFactory: beforeAppSerialized, multi: true, deps: [ DOCUMENT ]
-          }
+        [{
+          provide: ORIGIN_URL,
+          useValue: options.request.origin
+        }, {
+          provide: REQUEST,
+          useValue: options.request.data.request
+        }
         ]
       );
 
       getFactory(moduleOrFactory, compiler)
         .then(factory => {
-          return renderModuleFactory(factory, {
+          return renderModuleFactory2(factory, {
             document: options.appSelector,
             url: options.request.url,
             extraProviders: extraProviders
           });
         })
-        .then(() => {
+        .then(doc => {
+
+          const universalData = _getUniversalData(doc);
 
           resolve({
-            html: UniversalData.appNode,
+            html: universalData.appNode,
             globals: {
-              styles: UniversalData.styles,
-              title: UniversalData.title,
-              scripts: UniversalData.scripts,
-              meta: UniversalData.meta,
-              links: UniversalData.links
+              styles: universalData.styles,
+              title: universalData.title,
+              scripts: universalData.scripts,
+              meta: universalData.meta,
+              links: universalData.links
             }
 
           });
